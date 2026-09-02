b37b2bef981f1d35a131cae95fa7b55c919caa10
hutatBoosty <Parfenov.max24@yandex.ru>
2026-09-03 00:06:41 +0700

    add example test

---
+++ src/test/java/org/example/TestClass.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    public void checkTest() {
+        Assertions.assertTrue(true);
+    }
+}
