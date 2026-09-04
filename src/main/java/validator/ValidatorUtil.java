e558381b5e18d370c7f0bfdbaa529f546ec797d8
ira-razin4ova <ipopova024@gmail.com>
2026-09-04 13:55:13 +0300

    SCRUM-12 creating a data validator  add description ValidatorUtil

--- src/main/java/validator/ValidatorUtil.java
+++ src/main/java/validator/ValidatorUtil.java
@@ -6,10 +6,21 @@ import jakarta.validation.Validator;
 
 import java.util.Set;
 
+/**
+ * Утилитный класс для проверки объектов на соответствие ограничениям Jakarta Bean Validation.
+ * Предоставляет методы для валидации полей по аннотациям (например, @NotNull, @Min).
+ */
 public class ValidatorUtil {
 
     private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
 
+    /**
+     * Проверяет переданный объект на наличие нарушений ограничений валидации.
+     *
+     * @param <T> тип проверяемого объекта
+     * @param object объект, который необходимо провалидировать
+     * @throws IllegalArgumentException если объект не прошел валидацию (текст исключения содержит сообщение первого найденного нарушения)
+     */
     public static <T> void validate(T object) {
         Set<ConstraintViolation<T>> violations = VALIDATOR.validate(object);
         if (!violations.isEmpty()) {
