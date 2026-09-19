2946912878e34834282e0ad6e71526daecd14587
hutatBoosty <Parfenov.max24@yandex.ru>
2026-09-19 18:14:56 +0700

    SCRUM-22 Add @Validated to UserController

--- src/main/java/org/example/spring/controllers/UserController.java
+++ src/main/java/org/example/spring/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.example.spring.dto.UserDto;
 import org.example.spring.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
     private final UserService userService;
 
